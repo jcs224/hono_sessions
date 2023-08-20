61c8777357d67eb8e11f78dfb68bbf40e8886e7c
Joe Sweeney <joe.sweeney224@gmail.com>
2023-08-20 17:46:35 -0600

    fix npm build

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { nanoid } from 'https://esm.sh/nanoid@4.0.0/async'
 export type { MiddlewareHandler, Context } from 'https://deno.land/x/hono@v3.4.3/mod.ts'
-export { getCookie, setCookie } from 'https://deno.land/x/hono@v3.4.3/middleware.ts'
+export { getCookie, setCookie } from 'https://deno.land/x/hono@v3.4.3/middleware/cookie/index.ts'
--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -25,10 +25,22 @@ await build({
       url: "https://github.com/jcs224/hono-sessions/issues",
     },
   },
-  // typeCheck: false,
+  typeCheck: false,
   // scriptModule: false,
   compilerOptions: {
     lib: ['DOM', 'ES2022']
+  },
+
+  mappings: {
+    'https://deno.land/x/hono@v3.4.3/mod.ts': {
+      name: 'hono',
+      version: "^3.4.3",
+    },
+    'https://deno.land/x/hono@v3.4.3/middleware/cookie/index.ts': {
+      name: 'hono',
+      version: "^3.4.3",
+      subPath: 'cookie'
+    }
   }
 });
 
