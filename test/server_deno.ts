fec42ed89971dd27eb8a78030a57ff24cd9289f2
Joe Sweeney <joe.sweeney224@gmail.com>
2023-09-12 16:22:21 -0600

    test encrypted cookie

--- test/server_deno.ts
+++ test/server_deno.ts
@@ -1,5 +1,5 @@
 import { createHonoApp } from './setup_app.ts'
 
-const app = await createHonoApp()
+const app = await createHonoApp('memory', 'password_at_least_32_characters_long')
 
 Deno.serve(app.fetch)
--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,6 @@
 import { createHonoApp } from './setup_app.ts'
 import { assert, assertEquals } from 'https://deno.land/std@0.200.0/assert/mod.ts'
+import { encrypt, decrypt } from '../src/Crypto.ts'
 
 Deno.test('Memory Store: establish session', async () => {
   const app = await createHonoApp('memory')
@@ -20,4 +21,33 @@ Deno.test('Memory Store: establish session', async () => {
   assert(nextSessionCookies.length > 0)
 
   assertEquals(sessionCookies[0], nextSessionCookies[0])
+})
+
+Deno.test('Memory Store: establish session with encryption', async () => {
+  const key = 'password_at_least_32_characters_long'
+
+  const app = await createHonoApp('memory', key)
+  const res = await app.request('/')
+
+  const sessionCookies = res.headers.getSetCookie().filter(entry => entry.includes('session='))
+
+  assert(sessionCookies.length > 0)
+
+  const session_id_encrypted = sessionCookies[0].split('session=')[1].split('; SameSite=Lax')[0]
+  const sessionId = await decrypt(key, session_id_encrypted) as string
+
+  const next_res = await app.request('/', {
+    headers: {
+      'Cookie': 'session=' + (await encrypt(key, sessionId))
+    }
+  })
+
+  const nextSessionCookies = next_res.headers.getSetCookie().filter(entry => entry.includes('session='))
+
+  assert(nextSessionCookies.length > 0)
+
+  const next_session_id_encrypted = nextSessionCookies[0].split('session=')[1].split('; SameSite=Lax')[0]
+  const nextSessionId = await decrypt(key, next_session_id_encrypted) as string
+
+  assertEquals(sessionId, nextSessionId)
 })
