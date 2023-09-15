0e9edaa434141b4ca322a5f81d40c455b435b329
Joe Sweeney <joe.sweeney224@gmail.com>
2023-09-14 19:03:08 -0600

    set up test server

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -69,9 +69,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'deno run -A ../test/server_deno.ts',
+    url: 'http://127.0.0.1:8000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
