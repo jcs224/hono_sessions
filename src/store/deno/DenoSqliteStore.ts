c9caf8ca44db034b18c156c061faed77c6c31da2
Joe Sweeney <joe.sweeney224@gmail.com>
2023-09-16 00:23:16 -0600

    add deno sqlite store, clean up tests

---
+++ src/store/deno/DenoSqliteStore.ts
@@ -0,0 +1,38 @@
+import Store from '../Store.ts'
+import { SessionData } from '../../Session.ts'
+import { DB } from 'https://deno.land/x/sqlite@v3.8/mod.ts'
+
+export class DenoSqliteStore implements Store {
+  db: DB
+  tableName: string
+
+  constructor(db : DB, tableName = 'sessions') {
+    this.db = db
+    this.tableName = tableName
+    this.db.query(`CREATE TABLE IF NOT EXISTS ${this.tableName} (id TEXT NOT NULL PRIMARY KEY, data TEXT)`)
+  }
+
+  getSessionById(sessionId : string) {
+    let session = ''
+    
+    for (const [sess] of this.db.query<string[]>(`SELECT data FROM ${this.tableName} WHERE id = ?`, [sessionId])) {
+      session = sess
+    }
+
+    return session ? JSON.parse(session) as SessionData : null;
+  }
+
+  createSession(sessionId : string, initialData : SessionData) {
+    this.db.query(`INSERT INTO ${this.tableName} (id, data) VALUES (?, ?)`, [sessionId, JSON.stringify(initialData)]);
+  }
+
+  deleteSession(sessionId : string) {
+    this.db.query(`DELETE FROM ${this.tableName} WHERE id = ?`, [sessionId])
+  }
+
+  persistSessionData(sessionId : string, sessionData : SessionData) {
+    this.db.query(`UPDATE ${this.tableName} SET data = ? WHERE id = ?`, [
+      JSON.stringify(sessionData), sessionId
+    ]);
+  }
+}
--- test/deno/MakeStore.ts
+++ test/deno/MakeStore.ts
@@ -1,5 +1,7 @@
-import { Store, CookieStore, MemoryStore } from "../../mod.ts";
-import { DenoKvStore } from "../../src/store/deno/DenoKvStore.ts";
+import { Store, CookieStore, MemoryStore } from '../../mod.ts'
+import { DenoKvStore } from '../../src/store/deno/DenoKvStore.ts'
+import { DenoSqliteStore } from '../../src/store/deno/DenoSqliteStore.ts'
+import { DB } from 'https://deno.land/x/sqlite@v3.8/mod.ts'
 
 export async function MakeDenoStore(storeDriver: string | undefined): Promise<Store | CookieStore> {
   let store: Store | CookieStore
@@ -14,6 +16,11 @@ export async function MakeDenoStore(storeDriver: string | undefined): Promise<St
     case 'kv':
       const kv = await Deno.openKv('./tmp/sessions')
       store = new DenoKvStore(kv)
+      break
+    case 'sqlite':
+      const sqlite = new DB('./tmp/sessions.sqlite')
+      store = new DenoSqliteStore(sqlite)
+      break
     default:
       store = new MemoryStore()
       break
