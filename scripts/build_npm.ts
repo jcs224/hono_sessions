eb1bd0c6ec340c986acfb4191f15d6fc676998ad
Joe Sweeney <joe.sweeney224@gmail.com>
2024-05-17 17:30:49 -0600

    github actions fix attempt

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -42,3 +42,4 @@ await build({
 // post build steps
 Deno.copyFileSync("LICENSE", "npm/LICENSE");
 Deno.copyFileSync("README.md", "npm/README.md");
+Deno.copyFileSync("extras/.npmignore", "npm/.npmignore")
